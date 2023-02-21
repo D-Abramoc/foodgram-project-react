9d1d1b1ce3ff7eb0ba9cac018843d7e10036d807
Дмитрий Абрамов <elektrik.kem@yandex.ru>
2023-02-21 19:14:55 +0700

    swap HyperlinkSerialuzer to ModelSerializer

--- backend/backend_foodgram/api/serializers.py
+++ backend/backend_foodgram/api/serializers.py
@@ -4,25 +4,25 @@ from recipes.models import Ingredient, Recipe, Tag
 from users.models import CustomUser
 
 
-class CustomUserSerializer(serializers.HyperlinkedModelSerializer):
+class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'first_name', 'last_name')
 
 
-class IngredientSerializer(serializers.HyperlinkedModelSerializer):
+class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = '__all__'
 
 
-class RecipeSerializer(serializers.HyperlinkedModelSerializer):
+class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = '__all__'
 
 
-class TagSerializer(serializers.HyperlinkedModelSerializer):
+class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
