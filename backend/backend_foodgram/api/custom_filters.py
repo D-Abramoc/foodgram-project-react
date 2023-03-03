d2852399bf9fe449674db5ea576151225f906dec
Дмитрий Абрамов <elektrik.kem@yandex.ru>
2023-03-03 14:33:07 +0700

    fix IngredientFilter

--- backend/backend_foodgram/api/custom_filters.py
+++ backend/backend_foodgram/api/custom_filters.py
@@ -5,6 +5,8 @@ class IngredientFilter(filters.BaseFilterBackend):
     allowed_fields = ('name',)
 
     def filter_queryset(self, request, queryset, view):
+        if 'search' not in request.query_params:
+            return queryset
         desired = request.query_params['search']
         queryset = queryset.filter(name__istartswith=desired).order_by('name')
         return queryset
