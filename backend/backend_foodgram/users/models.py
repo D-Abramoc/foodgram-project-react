d4998ad3160fa7cced7e1a417e85a67b4fb9203b
Дмитрий Абрамов <elektrik.kem@yandex.ru>
2023-03-25 15:58:38 +0700

    delete role field from CustomUser model

--- backend/backend_foodgram/users/models.py
+++ backend/backend_foodgram/users/models.py
@@ -2,11 +2,6 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-USERS_ROLE = (
-    ('user', 'Пользователь'),
-    ('admin', 'Администратор')
-)
-
 
 class CustomUser(AbstractUser):
     username = models.CharField(
@@ -26,8 +21,6 @@ class CustomUser(AbstractUser):
     password = models.CharField(
         max_length=settings.MAX_LENGTH_CHARFIELD_CUSTOMUSER
     )
-    role = models.CharField(max_length=15, choices=USERS_ROLE,
-                            default='user', blank=True)
     REQUIRED_FIELDS = ('first_name', 'last_name')
     USERNAME_FIELD = 'username'
 
@@ -38,14 +31,6 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
-
-    @ property
-    def is_user(self):
-        return self.role == USERS_ROLE[0][0]
-
-    @ property
-    def is_admin(self):
-        return self.is_superuser or self.role == USERS_ROLE[1][0]
 
 
 class Subscribe(models.Model):
