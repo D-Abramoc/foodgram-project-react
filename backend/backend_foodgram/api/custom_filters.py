0f2a900a2df796db20121fc06a7c72e8c213b74b
Abramov Dmitry <abramovdv42@gmail.com>
2023-03-28 07:50:05 +0000

    swap search to name in IngredientFilter

--- backend/backend_foodgram/api/custom_filters.py
+++ backend/backend_foodgram/api/custom_filters.py
@@ -11,9 +11,9 @@ class IngredientFilter(filters.BaseFilterBackend):
     allowed_fields = ('name',)
 
     def filter_queryset(self, request, queryset, view):
-        if 'search' not in request.query_params:
+        if 'name' not in request.query_params:
             return queryset
-        desired = request.query_params['search']
+        desired = request.query_params['name']
         return queryset.filter(name__istartswith=desired).order_by('name')
 
 
